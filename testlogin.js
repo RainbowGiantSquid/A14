cac6bf5d306fbe65e777a4b8bfa5025394278a7f
RainbowGiantSquid <38047481+RainbowGiantSquid@users.noreply.github.com>
2019-01-10 15:53:26 +0000

    Fiirst upload

---
+++ testlogin.js
@@ -0,0 +1,18 @@
+
+function dropdownMenu() {
+    var x = document.getElementById("dropdownClick");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
