15d8eb35902dc051d1d2c4f98094876c27f8fe1c
Dagmara <dagmarakukla@gmail.com>
2019-01-11 20:50:54 +0000

    separating js to another doc and more
    New flex container and adjustment for the percent charts, new animation
    
    text styling and new content update

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -7,11 +7,9 @@ function dropdownMenu() {
         x.className = "topnav";
     }
 }
-// Get the modal
-var modal = document.getElementById('id01');
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+$('.topnav li a').click(function() {
+    $('#dropdownClick').removeClass(' responsive');
+    });
+
+
+    
